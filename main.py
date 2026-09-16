3cc9f1865c2418b73a4a98abd72c485159d56d29
Amrita <amritakumari24@navgurukul.org>
2026-09-16 12:16:57 +0530

    CRUD operation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Request
 from data import products
+from dtos import ProductDTO
 app = FastAPI()
 
 @app.get("/")
@@ -34,3 +35,34 @@ def greet_user(request: Request):
         "greet": f"Hello {query_params.get("name")},your age is {query_params.get("age")} "
 
     }
+
+
+@app.post("/create_product")
+def create_product(product_data: ProductDTO):
+    product_data = product_data.model_dump()
+    products.append(product_data)
+    return products
+
+
+@app.put("/update_product/{product_id}")
+def update_product(product_data: ProductDTO, product_id:int):
+    for index, oneProduct in enumerate(products):
+        if oneProduct.get("id") == product_id:
+            products[index] = product_data.model_dump()
+            return {"status": "product updated successfully!", "product":product_data}
+        
+    return {
+         "error" : "Product not found for this id!"
+    }
+
+
+    
+@app.delete("/delete_product/{product_id}")
+def delete_product(product_id: int):
+    for index, oneProduct in enumerate(products):
+        if oneProduct.get("id") == product_id:
+            deleted_product = products.pop(index)
+            return {"status": "successfully deleted!", "product": deleted_product}
+    return {
+ "error": "Product not found for this id!"
+}        
